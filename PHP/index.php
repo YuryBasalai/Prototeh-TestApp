d2bc97ad990907f7c27c5e1678e67bb71b03778c
YuryBasalai <yurybasalai@gmail.com>
2020-06-22 09:50:49 +0300

    String replacement

--- PHP/index.php
+++ PHP/index.php
@@ -2,13 +2,16 @@
 $s = 'Example Text String';
 echo "<i>Original string:</i><br />".$s."<br />";
 
+
 echo "<br /><i>Replacing all T letters with N:</i><br />";
 echo str_replace('T', 'N', $s)."<br />";
+
 
 echo "<br /><i>Array of words:</i><br />";
 $array_word = explode(" ", $s);
 print_r($array_word);
 echo "<br />";
+
 
 echo "<br /><i>Case replacement:</i><br />";
 foreach (str_split($s) as $symbol) {
@@ -21,3 +24,20 @@ foreach (str_split($s) as $symbol) {
 }
 echo "<br />";
 ?>
+
+<?echo "<br /><i>String replacement:</i><br />";?>
+<form action="" method="get">
+	<input type="text" name="string" placeholder="New string"/><br />
+	<input type="submit" value="Replace" />
+	<input type="button" value="Reset" onclick="location.href = '/'"/>
+</form>
+
+<?
+if ($_GET) {
+	$new_string = $_GET;
+	$s = $new_string["string"];
+}
+echo "<b>".$s."</b>";
+?>
+
+
