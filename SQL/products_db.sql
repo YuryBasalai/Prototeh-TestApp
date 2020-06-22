c3bdf2039823cfcf1a7fc0241304b7b411e80559
YuryBasalai <yurybasalai@gmail.com>
2020-06-22 09:53:13 +0300

    creating records in a table

---
+++ SQL/products_db.sql
@@ -0,0 +1,7 @@
+SELECT * FROM products_db.products;
+/*INSERT INTO products (name, kind, price) VALUES
+	('bread', '1', 10),
+	('grape', 'kg', 60),
+	('milk', '1', 15),
+	('cookies', 'kg', 55),
+	('cucumbers', 'kg', 45);*/
