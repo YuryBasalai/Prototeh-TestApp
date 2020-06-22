6b72ee8bc464dfb503d2be271d91244516dfa5d2
YuryBasalai <yurybasalai@gmail.com>
2020-06-22 09:44:09 +0300

    function color

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,24 @@
 let span = document.createElement('span');
 span.innerHTML = "Example";
 example.append(span);
+
+
+
+
+color = () => {
+	switch (event.toElement) {
+		case span:
+			modal_color.style.display = "block";
+			modal_color.getElementsByTagName("p")[0].innerHTML = 'Цвет фона: ' + RgbToHex(window.getComputedStyle(example, null).getPropertyValue("background-color"))
+			break;
+		case example:
+			example.style.background = "#0000FF";
+			break;
+	}
+}
+
+example.addEventListener('click', {
+	handleEvent(event) {
+		color();
+	}
+});
