5158ed59d634f28cb2ebf1c13bee00e04ea52768
YuryBasalai <yurybasalai@gmail.com>
2020-06-22 09:49:43 +0300

    Array of words

--- PHP/index.php
+++ PHP/index.php
@@ -4,3 +4,8 @@ echo "<i>Original string:</i><br />".$s."<br />";
 
 echo "<br /><i>Replacing all T letters with N:</i><br />";
 echo str_replace('T', 'N', $s)."<br />";
+
+echo "<br /><i>Array of words:</i><br />";
+$array_word = explode(" ", $s);
+print_r($array_word);
+echo "<br />";
