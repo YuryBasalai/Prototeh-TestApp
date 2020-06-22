da75c4fd0e60690af4b9e6e0078892708d1f1c91
YuryBasalai <yurybasalai@gmail.com>
2020-06-22 09:49:20 +0300

    Replacing all T letters with N

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,6 @@
 <?
 $s = 'Example Text String';
 echo "<i>Original string:</i><br />".$s."<br />";
+
+echo "<br /><i>Replacing all T letters with N:</i><br />";
+echo str_replace('T', 'N', $s)."<br />";
